b80a942fcc1ad999e3bf5f87eea6f1b9e7beb207
Stella-Ndirangu <stellaw.ndirangu@gmail.com>
2018-12-03 17:00:20 +0300

    Add IF statement to check if all values are greater than 0

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ var triangle = function() {
     var side2 = document.getElementById('side2').value;
     var side3 = document.getElementById('side3').value;
 
+if (side1 > 0 && side2 > 0 && side3 > 0) {
     if (side1 + side2 >= side3 && side1 + side3 >= side2 && side2 + side3 >= side1) {
       if (side1 === side2 && side2 === side3 && side1 === side3) {
            document.getElementById("display").innerHTML = "This is an Equilateral Triangle";
@@ -14,4 +15,8 @@ var triangle = function() {
     } else {
         document.getElementById("display").innerHTML = "This is not a triangle!";
       }
-    };
+    } else
+    {
+      alert("Values of a triangle have to be greater than 0")
+    }
+  };
