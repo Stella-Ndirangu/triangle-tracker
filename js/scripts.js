a95412c05a5f012746aa82cc3ca31c42ea430505
Stella-Ndirangu <stellaw.ndirangu@gmail.com>
2018-12-03 13:15:58 +0300

    Declare Variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,5 @@
 var triangle = function(){
+  var side1 = document.getElementById('side1').value;
+  var side2 = document.getElementById('side2').value;
+  var side3 = document.getElementById('side3').value;
 }
