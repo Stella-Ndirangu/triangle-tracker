f76adb5f423e873e7093be407282ae374e269207
Stella-Ndirangu <stellaw.ndirangu@gmail.com>
2018-12-03 14:38:29 +0300

    Add if statements to js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,25 @@
-var triangle = function(){
-  var side1 = document.getElementById('side1').value;
-  var side2 = document.getElementById('side2').value;
-  var side3 = document.getElementById('side3').value;
-}
+var triangle = function() {
+    var side1 = document.getElementById('side1').value;
+    var side2 = document.getElementById('side2').value;
+    var side3 = document.getElementById('side3').value;
+
+    if (side1 + side2 >= side3 && side1 + side3 >= side2 && side2 + side3 >= side1) {
+      if (side1 === side2 && side2 === side3 && side1 === side3) {
+           document.getElementById("display").innerHTML = "This is an Equilateral Triangle";
+         } else if (side1 === side2 || side2 === side3 || side1 === side3) {
+               document.getElementById("display").innerHTML = "This is an Isosceles triangle";
+             } else {
+                   document.getElementById("display").innerHTML = "This is an Scalene triangle";
+                 }
+    } else {
+        document.getElementById("display").innerHTML = "This is not a triangle!";
+      }
+
+//    if (side1 === side2 && side2 === side3 && side1 === side3) {
+//      document.getElementById("display").innerHTML = "This is an Equilateral Triangle";
+//    } else if (side1 === side2 || side2 === side3 || side1 === side3) {
+//      document.getElementById("display").innerHTML = "This is an Isosceles triangle";
+//    } else {
+//      document.getElementById("display").innerHTML = "This is an Scalene triangle";
+//    }
+    };
