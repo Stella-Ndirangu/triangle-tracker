a9a4b81f8991a7441e64c958090aa4a698b820c7
Stella-Ndirangu <stellaw.ndirangu@gmail.com>
2018-12-03 20:28:03 +0300

    Correct typos, add styling to p and change variable names

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,21 @@
 var triangle = function() {
-    var side1 = document.getElementById('side1').value;
-    var side2 = document.getElementById('side2').value;
-    var side3 = document.getElementById('side3').value;
+  var side1 = parseInt(document.getElementById('side1').value);
+  var side2 = parseInt(document.getElementById('side2').value);
+  var side3 = parseInt(document.getElementById('side3').value);
 
-if (side1 > 0 && side2 > 0 && side3 > 0) {
+  if (side1 > 0 && side2 > 0 && side3 > 0){
     if (side1 + side2 >= side3 && side1 + side3 >= side2 && side2 + side3 >= side1) {
-      if (side1 === side2 && side2 === side3 && side1 === side3) {
-           document.getElementById("display").innerHTML = "This is an Equilateral Triangle";
-         } else if (side1 === side2 || side2 === side3 || side1 === side3) {
-               document.getElementById("display").innerHTML = "This is an Isosceles triangle";
-             } else {
-                   document.getElementById("display").innerHTML = "This is an Scalene triangle";
-                 }
-    } else {
-        document.getElementById("display").innerHTML = "This is not a triangle!";
+    if (side1 === side2 && side2 === side3 && side1 === side3) {
+        document.getElementById("display").innerHTML = "This is an Equilateral Triangle";
+      } else if (side1 === side2 || side1 === side3 || side2 === side3) {
+        document.getElementById("display").innerHTML = "This is an Isosceles Triangle";
+      } else {
+        document.getElementById("display").innerHTML = "This is an Scalene Triangle";
       }
-    } else
-    {
-      alert("Values of a triangle have to be greater than 0")
-    }
+    } else {
+      document.getElementById("display").innerHTML = "This is not a triangle!";
+    };
+  } else {
+    alert("Values of a triangle have to be greater than 0");
   };
+};
