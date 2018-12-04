fa4d9cd15078a130ea271464a0c7e98d985a11ed
Stella-Ndirangu <stellaw.ndirangu@gmail.com>
2018-12-04 14:43:57 +0300

    Changed indentation to make it uniform and consistent

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,9 @@ var triangle = function() {
   var side2 = parseInt(document.getElementById('side2').value);
   var side3 = parseInt(document.getElementById('side3').value);
 
-  if (side1 > 0 && side2 > 0 && side3 > 0){
+  if (side1 > 0 && side2 > 0 && side3 > 0) {
     if (side1 + side2 >= side3 && side1 + side3 >= side2 && side2 + side3 >= side1) {
-    if (side1 === side2 && side2 === side3 && side1 === side3) {
+      if (side1 === side2 && side2 === side3 && side1 === side3) {
         document.getElementById("display").innerHTML = "This is an Equilateral Triangle";
       } else if (side1 === side2 || side1 === side3 || side2 === side3) {
         document.getElementById("display").innerHTML = "This is an Isosceles Triangle";
